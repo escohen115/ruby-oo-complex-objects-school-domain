04a3369a7aaa7d34c9630f1f7b52adbe30692149
escohen115 <escohen115@gmail.com>
2020-10-28 08:56:08 -0400

    commit tree 9eef2881d9fbd01f09c361730e20db1501fd242a

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,18 @@
-# code here!
+class School
+    attr_accessor :name, :roster
+    def initialize (name)
+        @name = name
+        roster = {}
+    end
+
+    def add_student(name, grade)
+        name.to_s
+        grade.to_i
+            if !roster.has_value?(grade)
+                roster[grade]=[]
+            end
+        roster[grade]<<name
+    end
+
+end
+
